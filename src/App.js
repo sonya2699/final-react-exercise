d3aab875916a54e8b0aac99e215838de2d644f5c
Student <student@Cumberlandss-MacBook-Pro.local>
2021-11-29 22:29:36 -0600

    updated some files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+// 2a
 import MCUShows from "./mcu-shows/MCUShows";
 
-import {useState} from 'react'
+// BONUS 6a & 8a
+import { useState, useEffect } from 'react';
 
-const realeasDates = {
-  wandaVision: 'Jan 2021',
-  falconWinter: 'Mar 2021',
+// 5a
+const releaseDates = {
+  wandaVision: 'January 2021',
+  falconWinter: 'March 2021',
   loki: 'June 2021',
-  whatIf: 'August 2021',
-  hawkeye: 'November 2021'
-}
+  whatIf: 'August 2021',
+  hawkeye: 'November 2021'
+};
+
+// BONUS 6b
 const mcuCharacters = [
   'Scarlet Witch',
   'Vision',
@@ -17,22 +22,50 @@ const mcuCharacters = [
   'Loki',
   'The Watcher',
   'Hawkeye'
- ];
+];
 
 function App() {
-  const [index, setIndex]=useState(0);
+  // BONUS 6c
+  const [index, setIndex] = useState(0);
+
+  // BONUS 7a
   const [num, setNum] = useState(0);
 
+  // BONUS 8a
+  // 
   useEffect(() => {
-    alert('The next MCU character has been displayed')
-      }, [num]);
+    alert("The Next MCU Character has been displayed");
+  }, [num]);
+
+  // BONUS 6f
+  function newIndex() {
+    const randomIndex = Math.floor(Math.random() * mcuCharacters.length);
+    setIndex(randomIndex);
+  }
+
+  // BONUS 7d
+  function newNum() {
+    if (num < mcuCharacters.length - 1) {
+      setNum(num + 1);
+    } else {
+      setNum(0);
+    }
+  }
 
   return (
     <div>
-      <h1>"FINAL REACT EXERCISE"</h1>
-      <MCUShows dates={realeasDates}randomMCUCharacter={randomMCUCharacter}
-     />
-     <h1>Random MCU Character: {mcuCharacters[index]}</h1>
+      {/* 2b */}
+      <h1>FINAL REACT EXERCISE</h1>
+      {/* 2c & 5b & BONUS 6f & 7d */}
+      <MCUShows
+        dates={releaseDates}
+        indexTwo={newIndex}
+        numTwo={newNum}
+      />
+      {/* BONUS 6d & 6f */}
+      <h1>Random MCU Character: {mcuCharacters[index]}</h1>
+      {/* BONUS 7b & 7d */}
+      <h1>Next MCU Character: {mcuCharacters[num]}</h1>
     </div>
   );
 }
--- src/mcu-shows/MCUShows.js
+++ src/mcu-shows/MCUShows.js
@@ -1,36 +1,30 @@
+// FINAL REACT EXERCISE
+
+// 1a
 import './MCUShows.css';
 
-import React from 'react'
-
-const MCUShows = ({ dates, randomChar, charList, currentChar, nextChar }) => {
-
-    const rollRandomChar = () => {
-        let newChar = Math.floor(Math.random() * charList);
-        randomChar(newChar)
-    }
-    
-    const nextCharacter = () => {
-        if (currentChar === charList - 1) {
-            nextChar(0);
-        } else {
-            nextChar(currentChar + 1);
-        }
-    }
-    return(
-       <div className='MCUShows'>
-        <h1 style={{fontFamily:'Georgia',textDecoration: 'Underline'}}>Disney+</h1>
-        <h2>MCU Show Release Dates</h2>
-        <p> WandaVision: {dates.wandaVision}
-        </p>
-        <p>"The Falcon and the Winter Soldier: {dates.falconWinter}  </p>
-        <p>Loki: {dates.Loki} </p>
-        <p>What If...?: {dates.whatIf}
-        </p>
-        <p>Hawkeye: {dates.hawkeye} </p>
-        <button>Random MCU Character</button>
-        </div>
-
-    );
+// 1b & 5c & BONUS 6f & 7d
+function MCUShows({dates, indexTwo, numTwo}){
+  // 1c
+  return(
+    // 4a
+    <div className="MCUShows">
+      {/* 3a */}
+      <h1 style={{fontFamily: 'Georgia', textDecoration: 'underline'}}>Disney+</h1>
+      <h2>MCU Show Release Dates</h2>
+      {/* 5c */}
+      <p>WandaVision: {dates.wandaVision}</p>
+      <p>The Falcon and the Winter Soldier: {dates.falconWinter}</p>
+      <p>Loki: {dates.loki}</p>
+      <p>What If...?: {dates.whatIf}</p>
+      <p>Hawkeye: {dates.hawkeye}</p>
+      {/* BONUS 6e & 6f */}
+      <button onClick={indexTwo}>RANDOM MCU CHARACTER</button>
+      {/* BONUS 7c & 7d */}
+      <button onClick={numTwo}>NEXT MCU CHARACTER</button>
+    </div>
+  );
 }
 
-export default MCUShows
+// 1d
+export default MCUShows;
